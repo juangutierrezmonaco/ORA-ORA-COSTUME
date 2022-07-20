a693a7fdfff387e2e9a7fdc575f7dd9dcf1e69b7
Juan Manuel Gutierrez Monaco <juan.monaco.gutierrez@gmail.com>
2022-07-20 20:36:39 -0300

    Comienzo de migrado a otras paginas

--- js/global.js
+++ js/global.js
@@ -63,12 +63,32 @@ function actualizarStock () {
 // Estas funciones simulan cómo se recupera la data de la base de datos
 function getCosplaysFromDB () {
     let cosplays = [];
+
     cosplays.push(new Cosplay("Nobara Kugisaki", "Jujutsu Kaisen", "Cosplay", 7000, 0, 10, 5, "./assets/images/cosplays/hechos-a-medida/jujutsu-nobara.png"));
     cosplays.push(new Cosplay("Rei Ayanami", "Evangelion", "Seifuku", 5500, 0, 2, 100, "./assets/images/cosplays/hechos-a-medida/evangelion-rei.png", true));
     cosplays.push(new Cosplay("Tradicional", "Anime", "Seifuku", 6000, 0, 100, 20, "./assets/images/cosplays/hechos-a-medida/school-girl.png"));
     cosplays.push(new Cosplay("Legión de Reconocimiento", "Shingeki no Kyojin", "Chaqueta", 6500, 10, 100, 0, "./assets/images/cosplays/hechos-a-medida/shingeki-chaqueta.png"));
     cosplays.push(new Cosplay("Legión de Reconocimiento", "Shingeki no Kyojin", "Saco", 9500, 25, 120, 5, "./assets/images/cosplays/hechos-a-medida/shigenki-saco.png", true));
-    cosplays.push(new Cosplay("Kaonashi", "El viaje de Chihiro", "Cosplay", 8000, 15, 40, 7, "./assets/images/cosplays/chihiro-noface.png", true));
+    cosplays.push(new Cosplay("Kaonashi", "El viaje de Chihiro", "Cosplay", 8000, 15, 40, 7, "./assets/images/cosplays/chihiro-noface.png"));
+    cosplays.push(new Cosplay("Legión de Reconocimiento", "Shingeki no Kyojin", "Cosplay", 8000, 0, 200, 20, "./assets/images/cosplays/snk-full.png"));
+    cosplays.push(new Cosplay("Kaguya Ootsutsuki", "Naruto Shippuden", "Cosplay", 12000, 20, 10, 15, "./assets/images/cosplays/naruto-kaguya.png"));
+    cosplays.push(new Cosplay("Ciel Phantomhive", "Kuroshitsuji", "Cosplay", 14000, 0, 5, 30, "./assets/images/cosplays/kuroshitsuji-ciel.png"));
+    cosplays.push(new Cosplay("Gintoki Sakata", "Gintama", "Cosplay", 12000, 0, 40, 50, "./assets/images/cosplays/gintama-gintoki.png"));
+    cosplays.push(new Cosplay("Sailor Moon", "Sailor Moon", "Cosplay", 10000, 20, 100, 100, "./assets/images/cosplays/sailormoon-sailormoon.png"));
+    cosplays.push(new Cosplay("Draken", "Tokyo Revengers", "Chaqueta", 3900, 0, 45, 55, "./assets/images/cosplays/tokyo-revengers-draken-chaqueta.png"));
+    cosplays.push(new Cosplay("Androide 18", "Dragon Ball Z", "Cosplay", 9000, 15, 25, 35, "./assets/images/cosplays/dbz-androide-18.png"));
+    cosplays.push(new Cosplay("Joline Cujoh", "Jojo's Bizarre Adventure", "Cosplay", 14000, 0, 10, 20, "./assets/images/cosplays/jojo-jolyne-cujoh.png"));
+    cosplays.push(new Cosplay("Kashuu Kiyomitsu", "Touken Ranbu", "Cosplay", 8000, 25, 5, 20, "./assets/images/cosplays/touken-ranbu-kashuu-kiyomitsu.png"));
+    cosplays.push(new Cosplay("Kirito", "Sword Art Online", "Cosplay", 10000, 0, 30, 40, "./assets/images/cosplays/sao-kirito.png"));
+    cosplays.push(new Cosplay("Raven", "Jóvenes Titanes", "Cosplay", 10000, 0, 45, 10, "./assets/images/cosplays/jovenes-titanes-raven.png"));
+    cosplays.push(new Cosplay("Tsukasa Yugi", "Jibaku Shonen", "Cosplay", 9500, 50, 20, 5, "./assets/images/cosplays/jibaku-shonen-tsukasa-yugi.png"));
+    cosplays.push(new Cosplay("Tanjiro Kamado", "Kimetsu No Yaiba", "Cosplay", 11000, 0, 60, 40, "./assets/images/cosplays/kimetsu-no-yaiba-tanjiro.png"));
+
+    if (!document.URL.includes("index.html")){  // Si no estoy en el index, la ruta es otra
+        for (const cosplay of cosplays) {
+            cosplay.imagen = "." + cosplay.imagen;
+        }
+    }
 
     return cosplays;
 }
